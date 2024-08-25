c1d478db6df4a4a0c7a637a01db4da73b620093e
ProGru <adamowskijad22@op.pl>
2024-08-25 12:17:12 +0200

    Wrap tools for tests in more convenient way

---
+++ Assets/GruInject/API/TestTools/GruInjectTestingTool.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using GruInject.API.Attributes;
+using GruInject.GruInject.Core.Injection;
+using GruInject.GruInject.Core.Registration;
+
+namespace GruInject.GruInject.API.TestTools
+{
+    public class GruInjectTestingTool : IDisposable
+    {
+        private readonly GruInjectForTests _gruInject;
+        private readonly InstanceContainer _container;
+        private readonly InstanceProviderForTest _instanceProvider;
+        private readonly ServiceLocator _serviceLocator;
+        private bool _started = false;
+
+        public GruInjectTestingTool()
+        {
+            _gruInject = new GruInjectForTests(
+                new List<Type>() {typeof(AutoSpawnAttribute)},
+                new List<Type>() {typeof(InjectAttribute)});
+            _container = new InstanceContainer();
+            _instanceProvider = new InstanceProviderForTest(
+                new InstanceProvider(
+                    false, 
+                    new InstanceCreator(), 
+                    _container, 
+                    typeof(RegisterAsSingleInstanceAttribute), 
+                    typeof(RegisterInstanceAttribute)));
+            
+            _serviceLocator = new ServiceLocator(
+                new List<Type>() 
+                    {typeof(InjectAttribute)},typeof(RegisterAsSingleInstanceAttribute), typeof(RegisterInstanceAttribute), 
+                false, true);
+        }
+
+        public T GetInstance<T>()
+        {
+            if (_started)
+            {
+                return _gruInject.GetInstance<T>();
+            }
+            return _serviceLocator.GetInstance<T>();
+        }
+        
+        public void AddFakeInstance<T>(T instance)
+        {
+            _instanceProvider.AddFakeInstance(instance);
+        }
+        
+        public void Start()
+        {
+            _started = true;
+            _gruInject.Start(_instanceProvider, _container);
+        }
+
+        public void Dispose()
+        {
+            _serviceLocator.Dispose();
+            _instanceProvider.ClearFakeInstances();
+            if (_started)
+                _gruInject.Stop();
+        }
+    }
+}
